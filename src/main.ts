514a6760d2b0aa511a0f4217ebb5e84224bf5a61
liem_nguyen_thanh <liemdonduong@gmail.com>
2024-02-22 22:06:47 +0700

    chore(package): add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,16 @@ async function bootstrap() {
       validationError: { target: false },
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Social media api')
+    .setDescription('The social media API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
