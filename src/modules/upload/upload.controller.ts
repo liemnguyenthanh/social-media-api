f6d32ece635eb62ecddda3ecb5a5da7daf1f3af9
liem_nguyen_thanh <liemdonduong@gmail.com>
2024-03-01 09:13:05 +0700

    feat(upload): servicer

---
+++ src/modules/upload/upload.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadService } from './upload.service';
+
+@Controller('upload')
+export class UploadController {
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  create(@UploadedFile() file) {
+    return this.uploadService.upload(file);
+  }
+
+  @Get()
+  findAll() {
+    return this.uploadService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.uploadService.findOne(+id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.uploadService.remove(+id);
+  }
+}
